525e488606fc2b1ead55aaf305cd85335f7af7ff
NoahNieh <noahnieh@163.com>
2024-04-10 01:27:08 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -14,11 +14,11 @@ fn main() {
         "rustc-env=TEST_FOO={}",
         timestamp
     );
-    println!("cargo:{}", your_command);
+    println!("cargo::{}", your_command);
 
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "rustc-cfg=feature=pass";
-    println!("cargo:{}", your_command);
+    let your_command = "rustc-cfg=pass";
+    println!("cargo::{}", your_command);
 }
